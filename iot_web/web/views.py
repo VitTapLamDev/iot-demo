9f6b2aaffee76f53785965022c00c4121cd6afd5
28.8vietnguyen <28.8vietnguyen@gmail.com>
2024-06-28 02:06:03 +0700

    Update

---
+++ iot_web/web/views.py
@@ -0,0 +1,75 @@
+from django.shortcuts import render
+from django.views.decorators.csrf import csrf_exempt
+from django.views.decorators.http import require_http_methods
+from django.http import JsonResponse
+
+import json
+import pymysql.cursors
+import os 
+from dotenv import load_dotenv
+
+load_dotenv()
+
+# Create your views here.
+@csrf_exempt
+def index(request):
+    return render(request, "web/index.html")
+
+@require_http_methods(["POST"])
+@csrf_exempt
+def sensor_data(request):
+    if request.method == "POST":
+        fire_alarm = False
+        body_content = json.loads(request.body)
+        
+        temperature = body_content["temperature"]
+        humidity    = body_content["humidity"]
+        lux         = body_content["lux"]
+        
+        print(f"temperature: {temperature}")
+        print(f"humidity: {humidity}")
+        print(f"lux: {lux}")
+        
+        if (temperature >= 60 and humidity <= 20) or (lux >= 1200):
+            fire_alarm = True
+        connection = pymysql.connect(
+            host=os.getenv('DATABASE_HOST'),
+            user=os.getenv('DATABASE_USERNAME'),
+            password=os.getenv('DATABASE_PASSWORD'),
+            database=os.getenv('DATABASE_NAME'),
+            charset='utf8mb4',
+            cursorclass=pymysql.cursors.DictCursor
+        )
+
+        print(connection)
+        with connection:
+            with connection.cursor() as cursor:
+                # Create a new record
+                sql = "INSERT INTO `sensor_data` (`temperature`, `humidity`, `lux`, `fire_alram`) VALUES (%s, %s, %s, %s)"
+                cursor.execute(sql, (temperature, humidity, lux, fire_alarm))
+            connection.commit()
+            
+        return JsonResponse({"text": "Succesfully send data !!!"})
+    else:
+        return JsonResponse({"text": "Method not allowed !!!"})
+        
+@csrf_exempt
+def alarm(request):
+    if request.method == "GET":
+        connection = pymysql.connect(
+            host=os.getenv('DATABASE_HOST'),
+            user=os.getenv('DATABASE_USERNAME'),
+            password=os.getenv('DATABASE_PASSWORD'),
+            database=os.getenv('DATABASE_NAME'),
+            charset='utf8mb4',
+            cursorclass=pymysql.cursors.DictCursor
+        )
+
+        print(connection)
+        with connection:
+            with connection.cursor() as cursor:
+                sql = "SELECT * FROM `sensor_data`"
+                cursor.execute(sql)
+                result = cursor.fetchone()
+        return JsonResponse(result)
+        
